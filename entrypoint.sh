2f88104870240dece68bec88ed9b69340079dcf4
Fernando <fernando.carvalho@paygo.com.br>
2020-10-28 16:21:36 -0300

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,6 +20,7 @@ CLONE_DIR=$(mktemp -d)
 echo "Cloning destination git repository"
 git config --global user.email "$INPUT_USER_EMAIL"
 git config --global user.name "$INPUT_USER_NAME"
+export GITHUB_TOKEN=$API_TOKEN_GITHUB
 git clone "https://$API_TOKEN_GITHUB@github.com/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
 git checkout -b "$INPUT_DESTINATION_BRANCH"
 
@@ -34,8 +35,8 @@ then
   git commit --message "Update from https://github.com/$GITHUB_REPOSITORY/commit/$GITHUB_SHA"
   echo "Pushing git commit"
   git push -u origin HEAD:$INPUT_DESTINATION_BRANCH
-  echo 'Authentication on git hub cli'
-  gh auth login
+  # echo 'Authentication on git hub cli'
+  # gh auth login
   echo "Creating a pull request"
   gh pr create --reviewer fernandodpcarvalho
 else
